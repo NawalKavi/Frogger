5c641c89f84540fc174aeb0ebfd86c79c1990702
Nawal <nawalkavi@gmail.com>
2023-03-29 23:00:27 +0100

    car collisions yippee!!!! (jesus christ kill me)
    All code marked in single quotations is EXPERIMENTAL.
    
    Added in car collisions, nearly *unalived* myself while trying to add in drowning and standing on logs.
    
    Probably my final commit for this project for the foreseeable future, unless I manage to develop the addiction I currently have to coding this again somehow.

--- main.py
+++ main.py
@@ -2,11 +2,11 @@
 import pygame
 from random import *
 
-from pygame.font import Font
-
 pygame.init()
 pygame.font.init()
 gameRunning = True
+deathCount = 0
+winCount = 0
 
 # Game window parameters.
 width = 320
@@ -19,11 +19,12 @@ pygame.display.set_caption("The Chronicles of Big Poppa")
 imageDictionary = {
     "frog": pygame.transform.scale(pygame.image.load("Images//Frog.png"), (30, 30)),
     "frogWin": pygame.transform.scale(pygame.image.load("Images//Crowned Frog.png"), (35, 35)),
-    "blueBike": pygame.transform.scale(pygame.image.load("Images//Blue Bike.png"), (40, 40)),
     "redCar": pygame.transform.scale(pygame.image.load("Images//Red Car.png"), (40, 40)),
+    "blueBike": pygame.transform.scale(pygame.image.load("Images//Blue Bike.png"), (40, 40)),
     "background": pygame.transform.scale(pygame.image.load("Images//Background.png"), (320, 480)),
     "heart": pygame.transform.scale(pygame.image.load("Images//Heart.png"), (25, 25)),
-    "log": pygame.transform.scale(pygame.image.load("Images//Log.png"), (100, 50))
+    "log": pygame.transform.scale(pygame.image.load("Images//Log.png"), (100, 50)),
+    "blackSquare": pygame.transform.scale(pygame.image.load("Images//Black.jpg"), (30, 30))
 }
 
 # Blitting or something.
@@ -52,6 +53,14 @@ class Frog:
     def __init__(self, frogX, frogY):
         self.frogX = frogX
         self.frogY = frogY
+        self.onLog = False
+        self.drowning = None
+
+    def currentX(self):
+        return self.frogX
+
+    def currentY(self):
+        return self.frogY
 
     def upMove(self):
         if self.frogY > 80:
@@ -60,8 +69,7 @@ class Frog:
             if self.frogY < 75:
                 self.frogY += 0
             else:
-                self.frogY -= 5
-        print("Frog Y:", self.frogY)
+                self.frogY -= 2
 
     def downMove(self):
         if self.frogY < 445:
@@ -69,8 +77,7 @@ class Frog:
             if self.frogY < 75:
                 self.frogY += 0
             else:
-                self.frogY += 5
-        print("Frog Y:", self.frogY)
+                self.frogY += 2
 
     def leftMove(self):
         if self.frogX >= 0:
@@ -78,8 +85,7 @@ class Frog:
             if self.frogY < 75:
                 self.frogX += 0
             else:
-                self.frogX -= 5
-        print("Frog X:", self.frogX)
+                self.frogX -= 2
 
     def rightMove(self):
         if self.frogX <= 290:
@@ -87,8 +93,7 @@ class Frog:
             if self.frogY < 75:
                 self.frogX += 0
             else:
-                self.frogX += 5
-        print("Frog X:", self.frogX)
+                self.frogX += 2
 
     def winPosition(self):
         if self.frogY < 81:
@@ -104,7 +109,32 @@ class Frog:
                 self.frogX = 230
                 self.frogY = 55
 
+    def moveFrogAway(self):
+        if car1.collisionDetection() or car2.collisionDetection() or car3.collisionDetection():
+            self.frogX = 310
+            self.frogY = 55
+    '''
+        def standOnLog(self):
+            if log1.collisionDetection():
+                self.onLog = True
+                self.frogX = log1.currentX()
+                self.frogY = log1.currentY()
+            elif log2.collisionDetection():
+                self.onLog = True
+                self.frogX = log2.currentX()
+                self.frogY = log2.currentY()
+            elif log3.collisionDetection():
+                self.onLog = True
+                self.frogX = log3.currentX()
+                self.frogY = log3.currentY()
+            else:
+                self.onLog = False
 
+            def drowningDetection(self):
+                if 78 < self.frogX < 240:
+                    if -1 < self.frogX < 291 and self.onLog == False:
+                        return False
+    '''
     def blitImage(self):
         mainWindow.blit(imageDictionary["frog"], (self.frogX, self.frogY))
 
@@ -119,53 +149,50 @@ moveRight = False
 moveUp = False
 moveDown = False
 
+
 class Car:
 
-    def __init__(self, carX, carY):
+    def __init__(self, carX, carY, direction, speed):
         self.carX = carX
         self.carY = carY
-
-    def keepMoving(self):
-        if gameRunning:
-            self.carX += 15
-
-    def moveCarBack(self):
-        if self.carX > 345:
-            self.carX = -30
-
-    def blitCar(self):
-        mainWindow.blit(imageDictionary["redCar"], (self.carX, self.carY))
-
-car = Car(-30, 280)
-
-
-class Bike:
-
-    def __init__(self, bikeX, bikeY, direction):
-        self.bikeX = bikeX
-        self.bikeY = bikeY
         self.direction = direction
+        self.speed = speed
 
     def keepMoving(self):
         if gameRunning:
             if self.direction == "Right":
-                self.bikeX += 10
+                self.carX += self.speed
             if self.direction == "Left":
-                self.bikeX -= 8
+                self.carX -= self.speed
 
-    def moveBikeBack(self):
+    def moveCarBack(self):
         if self.direction == "Right":
-            if self.bikeX > 345:
-                self.bikeX = -30
+            if self.carX > 345:
+                self.carX = -30
         if self.direction == "Left":
-            if self.bikeX < -50:
-                self.bikeX = 315
+            if self.carX < -50:
+                self.carX = 315
 
-    def blitBike(self):
-        mainWindow.blit(imageDictionary["blueBike"], (self.bikeX, self.bikeY))
+    def collisionDetection(self):
+        if self.carX < bigPoppa1.currentX() < (self.carX + 40):
+            if self.carY < bigPoppa1.currentY() < (self.carY + 40):
+                return True
+        if self.carX < bigPoppa2.currentX() < (self.carX + 40):
+            if self.carY < bigPoppa2.currentY() < (self.carY + 40):
+                return True
+        if self.carX < bigPoppa3.currentX() < (self.carX + 40):
+            if self.carY < bigPoppa3.currentY() < (self.carY + 40):
+                return True
 
-bike1 = Bike(335, 325, "Left")
-bike2 = Bike(-30, 370, "Right")
+    def blitCar(self):
+        if self.direction == "Left":
+            mainWindow.blit(imageDictionary["blueBike"], (self.carX, self.carY))
+        else:
+            mainWindow.blit(imageDictionary["redCar"], (self.carX, self.carY))
+
+car1 = Car(-30, 280, "Right", 2)
+car2 = Car(335, 325, "Left", 4)
+car3 = Car(-30, 370, "Right", 3)
 
 
 class Log:
@@ -191,17 +218,39 @@ class Log:
             if self.logX < -90:
                 self.logX = 350
 
+    '''
+        def collisionDetection(self):
+            if self.logX < bigPoppa1.currentX() < (self.logX + 100):
+                if self.logY < bigPoppa1.currentY() < (self.logY + 30):
+                    return True
+            if self.logX < bigPoppa2.currentX() < (self.logX + 100):
+                if self.logY < bigPoppa2.currentY() < (self.logY + 30):
+                    return True
+            if self.logX < bigPoppa3.currentX() < (self.logX + 100):
+                if self.logY < bigPoppa3.currentY() < (self.logY + 30):
+                    return True
+    '''
+
+    def currentX(self):
+        return self.logX
+
+    def currentY(self):
+        return self.logY
+
     def blitLog(self):
         mainWindow.blit(imageDictionary["log"], (self.logX, self.logY))
 
-log1 = Log(-80, 100, 9, "Right")
-log2 = Log(-80, 150, 5, "Right")
-log3 = Log(350, 205, 12, "Left")
+log1 = Log(-80, 100, 4, "Right")
+log2 = Log(-80, 150, 1, "Right")
+log3 = Log(350, 205, 2, "Left")
+
+
+# Frame rate.
+clock = pygame.time.Clock()
+endText = pygame.font.SysFont("monospace", 42)
 
 
 # Game loop.
-clock = pygame.time.Clock()
-endText = pygame.font.SysFont("monospace", 42)
 while gameRunning:
     for gameEvent in pygame.event.get():
 
@@ -275,17 +324,40 @@ while gameRunning:
     # Always blit images after drawing the initial background, because otherwise it'll just cover the images.
     blitImage(imageDictionary["background"], (0, 0))
 
-    # Hearts.
-    blitImage(imageDictionary["heart"], (290, 5))
-    blitImage(imageDictionary["heart"], (260, 5))
-    blitImage(imageDictionary["heart"], (230, 5))
-
     # River.
-    pygame.draw.rect(mainWindow, (7, 11, 120), pygame.Rect(00, 90, 320, 157))
+    river = pygame.draw.rect(mainWindow, (7, 11, 120), pygame.Rect(00, 90, 320, 157))
+
+    # Log 1.
+    log1.blitLog()
+    '''
+        log1.collisionDetection()
+    '''
+    log1.keepMoving()
+    log1.moveLogBack()
+
+    # Log 2.
+    log2.blitLog()
+    '''
+        log2.collisionDetection()
+    '''
+    log2.keepMoving()
+    log2.moveLogBack()
+
+    # Log 3.
+    log3.blitLog()
+    '''
+        log3.collisionDetection()
+    '''
+    log3.keepMoving()
+    log3.moveLogBack()
 
     # Frog 1.
     bigPoppa1.blitImage()
-    bigPoppa1.winPosition()
+    '''
+        bigPoppa1.drowningDetection()
+        bigPoppa1.standOnLog()
+        bigPoppa1.winPosition()
+    '''
     if moveUp:
         bigPoppa1.upMove()
     if moveDown:
@@ -294,10 +366,27 @@ while gameRunning:
         bigPoppa1.rightMove()
     if moveLeft:
         bigPoppa1.leftMove()
+    if car1.collisionDetection() or car3.collisionDetection() or car3.collisionDetection():
+        bigPoppa1.moveFrogAway()
+        blitImage(imageDictionary["blackSquare"], (230, 5))
+        deathCount += 1
+    '''
+        if log1.collisionDetection() or log2.collisionDetection() or log3.collisionDetection():
+            bigPoppa1.standOnLog()
+        if bigPoppa1.drowningDetection():
+            bigPoppa1.moveFrogAway()
+            blitImage(imageDictionary["blackSquare"], (230, 5))
+            deathCount += 1
+            print("collision")
+    '''
     if bigPoppa1.frogY < 60:
 
         # Frog 2.
         bigPoppa2.blitImage()
+        '''
+            bigPoppa2.drowningDetection()
+            bigPoppa2.standOnLog()
+        '''
         bigPoppa2.winPosition()
         if moveUp:
             bigPoppa2.upMove()
@@ -307,10 +396,27 @@ while gameRunning:
             bigPoppa2.rightMove()
         if moveLeft:
             bigPoppa2.leftMove()
+        if car1.collisionDetection() or car2.collisionDetection() or car3.collisionDetection():
+            bigPoppa2.moveFrogAway()
+            blitImage(imageDictionary["blackSquare"], (230, 5))
+            deathCount += 1
+        '''
+            if log1.collisionDetection() or log2.collisionDetection() or log3.collisionDetection():
+                bigPoppa2.standOnLog()
+            if bigPoppa2.drowningDetection():
+                bigPoppa2.moveFrogAway()
+                blitImage(imageDictionary["blackSquare"], (230, 5))
+                deathCount += 1
+                print("collision")
+        '''
         if bigPoppa2.frogY < 60:
 
             # Frog 3.
             bigPoppa3.blitImage()
+            '''
+                bigPoppa3.drowningDetection()
+                bigPoppa3.standOnLog()
+            '''
             bigPoppa3.winPosition()
             if moveUp:
                 bigPoppa3.upMove()
@@ -320,45 +426,56 @@ while gameRunning:
                 bigPoppa3.leftMove()
             if moveRight:
                 bigPoppa3.rightMove()
+            if car1.collisionDetection() or car2.collisionDetection() or car3.collisionDetection():
+                bigPoppa3.moveFrogAway()
+                blitImage(imageDictionary["blackSquare"], (230, 5))
+                deathCount += 1
+            '''
+                if log1.collisionDetection() or log2.collisionDetection() or log3.collisionDetection():
+                    bigPoppa3.standOnLog()
+                if bigPoppa3.drowningDetection():
+                    bigPoppa3.moveFrogAway()
+                    blitImage(imageDictionary["blackSquare"], (230, 5))
+                    deathCount += 1
+                    print("collision")
+            '''
+    # Hearts.
+    if deathCount == 0:
+        blitImage(imageDictionary["heart"], (290, 5))
+        blitImage(imageDictionary["heart"], (260, 5))
+        blitImage(imageDictionary["heart"], (230, 5))
+    if deathCount == 1:
+        blitImage(imageDictionary["heart"], (290, 5))
+        blitImage(imageDictionary["heart"], (260, 5))
+    if deathCount == 2:
+        blitImage(imageDictionary["heart"], (290, 5))
 
+    # Cars.
+    car1.blitCar()
+    car1.collisionDetection()
+    car1.keepMoving()
+    car1.moveCarBack()
 
+    car2.blitCar()
+    car2.collisionDetection()
+    car2.keepMoving()
+    car2.moveCarBack()
 
-    # Car.
-    car.blitCar()
-    car.keepMoving()
-    car.moveCarBack()
-
-    # Bike 1.
-    bike1.blitBike()
-    bike1.keepMoving()
-    bike1.moveBikeBack()
-
-    # Bike 2.
-    bike2.blitBike()
-    bike2.keepMoving()
-    bike2.moveBikeBack()
-
-    # Log 1.
-    log1.blitLog()
-    log1.keepMoving()
-    log1.moveLogBack()
-
-    # Log 2.
-    log2.blitLog()
-    log2.keepMoving()
-    log2.moveLogBack()
-
-    # Log 3.
-    log3.blitLog()
-    log3.keepMoving()
-    log3.moveLogBack()
+    car3.blitCar()
+    car3.collisionDetection()
+    car3.keepMoving()
+    car3.moveCarBack()
 
     # End screen.
     if bigPoppa1.frogY < 60 and bigPoppa2.frogY < 60 and bigPoppa3.frogY < 60:
         mainWindow.fill((0, 0, 0))
         displayText = endText.render("GAME OVER", 1, (255, 255, 255))
         mainWindow.blit(displayText, (50, 50))
+    if deathCount >= 3:
+        mainWindow.fill((0, 0, 0))
+        displayText = endText.render("GAME OVER", 1, (255, 255, 255))
+        mainWindow.blit(displayText, (50, 50))
 
     # Renders the game.
     pygame.display.flip()
-    clock.tick(15)
+    clock.tick(60)
